86838967d04560a789d8f40881bd062dd16f5517
abgf0103 <152011487+abgf0103@users.noreply.github.com>
2023-12-08 14:02:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>PHP</title>
-  </head>
-  <body>
 <?php
-    echo phpinfo();   
+    $conn = mysqli_connect( 'svc.sel4.cloudtype.app', 'root', 'root', 'testdb' );
+    $sql = "SELECT * FROM test_table LIMIT 5;";
+    $result = mysqli_query( $conn, $sql );
+    
+    while( $row = mysqli_fetch_array( $result ) ) {
+        echo
+        ''.$row['address'].';'.$row['lat'].';'.$row['lng'].':';
+      }
 ?>
-  </body>
-</html>
